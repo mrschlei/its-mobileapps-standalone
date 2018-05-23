f918ba2892f1ef07d56fb0fc914f5d702c64fa70
mrschlei <mrschlei@umich.edu>
2018-05-23 12:04:11 -0400

    total annihilation

--- start.sh
+++ start.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# Redirect logs to stdout and stderr for docker reasons.
-# it seems both sylinks already exist. 
-# these commands create unnecessary duplicates
 ln -sf /dev/stdout /var/log/apache2/access_log
 ln -sf /dev/stderr /var/log/apache2/error_log
 
@@ -10,24 +7,23 @@ ln -sf /dev/stderr /var/log/apache2/error_log
 ln -sf /secrets/apache2/apache2.conf /etc/apache2/apache2.conf
 ln -sf /secrets/apache2/default-ssl.conf /etc/apache2/sites-available/default-ssl.conf
 ln -sf /secrets/apache2/cosign.conf /etc/apache2/mods-available/cosign.conf
-#moving ports to local.start.sh
-#ln -sf /secrets/apache2/ports.conf /etc/apache2/ports.conf
+
 # SSL secrets
 ln -sf /secrets/ssl/USERTrustRSACertificationAuthority.pem /etc/ssl/certs/USERTrustRSACertificationAuthority.pem
 ln -sf /secrets/ssl/AddTrustExternalCARoot.pem /etc/ssl/certs/AddTrustExternalCARoot.pem
 ln -sf /secrets/ssl/sha384-Intermediate-cert.pem /etc/ssl/certs/sha384-Intermediate-cert.pem
 
-a2enmod ssl
-a2enmod include
-a2ensite default-ssl 
+if [ -f /secrets/app/local.start.sh ]
+then
+  /bin/sh /secrets/app/local.start.sh
+fi
 
 ## Rehash command needs to be run before starting apache.
 c_rehash /etc/ssl/certs >/dev/null
 
-if [ -f /secrets/app/local.start.sh ]
-then
-  /bin/bash /secrets/app/local.start.sh
-fi
+a2enmod ssl
+a2enmod include
+a2ensite default-ssl 
 
 #cd /var/www/html
 #drush @sites cc all --yes
